c28b1653aeeb74399ad9d7c564a6dd7bb2797062
yangzb <chaser8@163.com>
2021-11-23 10:05:04 +0800

    new

--- chaser-admin-api/src/main/java/top/chaser/admin/api/AdminApiApplication.java
+++ chaser-admin-api/src/main/java/top/chaser/admin/api/AdminApiApplication.java
@@ -3,6 +3,9 @@ package top.chaser.admin.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
 import top.chaser.framework.common.base.util.JSONUtil;
 
 import java.util.Properties;
@@ -11,8 +14,6 @@ import java.util.Properties;
 @Slf4j
 public class AdminApiApplication {
     public static void main(String[] args) {
-        Properties properties = System.getProperties();
-        log.error(JSONUtil.toPrettyString(properties));
         SpringApplication.run(AdminApiApplication.class, args);
     }
 }
